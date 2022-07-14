d20ece174effe038412d1d444a6bb7f239fec995
Tom Forbes <tom@tomforb.es>
2022-07-14 11:46:11 +0000

    {"name":"daxueba-net-scan","version":"2022.7.14.0","file":"daxueba_net_scan-2022.7.14.0-py3-none-any.whl","path":"daxueba-net-scan/daxueba_net_scan-2022.7.14.0-py3-none-any.whl"}

---
+++ daxueba-net-scan/daxueba_net_scan-2022.7.14.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""daxueba-net-scan
+https://github.com/apachecn/daxueba-net-scan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.7.14.0"
