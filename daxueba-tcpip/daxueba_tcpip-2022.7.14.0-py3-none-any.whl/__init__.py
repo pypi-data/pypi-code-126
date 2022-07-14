4df3eeab98690a43c2b5620757abf16ede0e1cd1
Tom Forbes <tom@tomforb.es>
2022-07-14 11:48:47 +0000

    {"name":"daxueba-tcpip","version":"2022.7.14.0","file":"daxueba_tcpip-2022.7.14.0-py3-none-any.whl","path":"daxueba-tcpip/daxueba_tcpip-2022.7.14.0-py3-none-any.whl"}

---
+++ daxueba-tcpip/daxueba_tcpip-2022.7.14.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""daxueba-tcpip
+https://github.com/apachecn/daxueba-tcpip"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.7.14.0"
