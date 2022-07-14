ee190d5ea5c1161aee52e8670071a9cb28d65151
Tom Forbes <tom@tomforb.es>
2022-07-14 11:37:54 +0000

    {"name":"daxueba-kali-wireless","version":"2022.7.14.0","file":"daxueba_kali_wireless-2022.7.14.0-py3-none-any.whl","path":"daxueba-kali-wireless/daxueba_kali_wireless-2022.7.14.0-py3-none-any.whl"}

---
+++ daxueba-kali-wireless/daxueba_kali_wireless-2022.7.14.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""daxueba-kali-wireless
+https://github.com/apachecn/daxueba-kali-wireless"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.7.14.0"
